3f5085e387cccd51e353cd0c7673ef62bee0d2bc
George  Dosher III <cgartist15@gmail.com>
2016-01-01 11:03:04 -0600

    Initial Commit

---
+++ SudokuSolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SudokuSolver
+//
+//  Created by George  Dosher III on 1/1/16.
+//  Copyright © 2016 George  Dosher III. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
